e8a082599dab1919d4471faab6b361bbe300c162
Thaddee Wiseur <twiseur@gmail.com>
2017-04-16 22:06:21 -0700

    Added homework1

---
+++ BirthdayCardGenerator/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BirthdayCardGenerator.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult BirthdayCardForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult BirthdayCardForm(Models.BirthdayCard birthdayCard)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("BirthdayCardSummary", birthdayCard);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ BirthdayCardGenerator/Models/BirthdayCard.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BirthdayCardGenerator.Models
+{
+    public class BirthdayCard
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string From { get; set; }
+
+        [Required(ErrorMessage = "Please enter the recipient's name")]
+        public string To { get; set; }
+
+        [Required(ErrorMessage = "Please enter your message")]
+        public string Message { get; set; }
+    }
+}
